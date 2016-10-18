a87a28c77b716dc154e30dae6d10bd94ce4aeaaf
ashanfernando <ashan256@gmail.com>
2016-10-18 21:17:17 +0530

    Filtering .json files when running executeAll method

--- dynamodb/migrations.js
+++ dynamodb/migrations.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var BbPromise = require('bluebird'),
-    fs = require('fs');
+    fs = require('fs'),
+    path = require('path');
 
-var createTable = function (dynamodb, migration) {
-    return new BbPromise(function (resolve) {
-        dynamodb.raw.createTable(migration.Table, function (err) {
+var createTable = function(dynamodb, migration) {
+    return new BbPromise(function(resolve) {
+        dynamodb.raw.createTable(migration.Table, function(err) {
             if (err) {
                 console.log(err);
             } else {
@@ -16,18 +17,18 @@ var createTable = function (dynamodb, migration) {
     });
 };
 
-var formatTableName = function (migration, options) {
+var formatTableName = function(migration, options) {
     return options.tablePrefix + migration.Table.TableName + options.tableSuffix;
 };
 
-var runSeeds = function (dynamodb, migration) {
+var runSeeds = function(dynamodb, migration) {
     if (!migration.Seeds || !migration.Seeds.length) {
-        return new BbPromise(function (resolve) {
+        return new BbPromise(function(resolve) {
             resolve(migration);
         });
     } else {
         var params,
-            batchSeeds = migration.Seeds.map(function (seed) {
+            batchSeeds = migration.Seeds.map(function(seed) {
                 return {
                     PutRequest: {
                         Item: seed
@@ -38,11 +39,11 @@ var runSeeds = function (dynamodb, migration) {
             RequestItems: {}
         };
         params.RequestItems[migration.Table.TableName] = batchSeeds;
-        return new BbPromise(function (resolve, reject) {
+        return new BbPromise(function(resolve, reject) {
             var interval = 0,
-                execute = function (interval) {
-                    setTimeout(function () {
-                        dynamodb.doc.batchWrite(params, function (err) {
+                execute = function(interval) {
+                    setTimeout(function() {
+                        dynamodb.doc.batchWrite(params, function(err) {
                             if (err) {
                                 if (err.code === "ResourceNotFoundException" && interval <= 5000) {
                                     execute(interval + 1000);
@@ -61,8 +62,8 @@ var runSeeds = function (dynamodb, migration) {
     }
 };
 
-var create = function (migrationName, options) {
-    return new BbPromise(function (resolve, reject) {
+var create = function(migrationName, options) {
+    return new BbPromise(function(resolve, reject) {
         var template = require('./templates/table.json');
         template.Table.TableName = migrationName;
 
@@ -70,7 +71,7 @@ var create = function (migrationName, options) {
             fs.mkdirSync(options.dir);
         }
 
-        fs.writeFile(options.dir + '/' + migrationName + '.json', JSON.stringify(template, null, 4), function (err) {
+        fs.writeFile(options.dir + '/' + migrationName + '.json', JSON.stringify(template, null, 4), function(err) {
             if (err) {
                 return reject(err);
             } else {
@@ -81,24 +82,26 @@ var create = function (migrationName, options) {
 };
 module.exports.create = create;
 
-var executeAll = function (dynamodb, options) {
-    return new BbPromise(function (resolve, reject) {
-        fs.readdirSync(options.dir).forEach(function (file) {
-            var migration = require(options.dir + '/' + file);
-            migration.Table.TableName = formatTableName(migration, options);
-            createTable(dynamodb, migration).then(function (executedMigration) {
-                runSeeds(dynamodb, executedMigration).then(resolve, reject);
-            });
+var executeAll = function(dynamodb, options) {
+    return new BbPromise(function(resolve, reject) {
+        fs.readdirSync(options.dir).forEach(function(file) {
+            if (path.extname(file) === ".json") {
+                var migration = require(options.dir + '/' + file);
+                migration.Table.TableName = formatTableName(migration, options);
+                createTable(dynamodb, migration).then(function(executedMigration) {
+                    runSeeds(dynamodb, executedMigration).then(resolve, reject);
+                });
+            }
         });
     });
 };
 module.exports.executeAll = executeAll;
 
-var execute = function (dynamodb, options) {
-    return new BbPromise(function (resolve, reject) {
+var execute = function(dynamodb, options) {
+    return new BbPromise(function(resolve, reject) {
         var migration = require(options.dir + '/' + options.migrationName + '.json');
         migration.Table.TableName = formatTableName(migration, options);
-        createTable(dynamodb, migration).then(function (executedMigration) {
+        createTable(dynamodb, migration).then(function(executedMigration) {
             runSeeds(dynamodb, executedMigration).then(resolve, reject);
         });
     });
